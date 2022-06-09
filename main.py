8b521df98540fbce2fd3aab5aadcf8137bb69e36
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-09 11:02:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ from streamlit_folium import st_folium
 import json
 from geopandas.tools import geocode
 
-bs = pd.read_csv("Bookshops.csv")
+books = pd.read_csv("Bookshops.csv")
+
+cat = st.selectbox(
+"Category", books["categories"].value_counts().index)
+df_selection = books[lambda x: x["Category"] == cat]
+df_selection
