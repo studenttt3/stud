85fec28e1f2e17bbe7348b99662c86a75f3c2675
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-09 10:50:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import streamlit as st
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from pywaffle import Waffle
+import folium
+from streamlit_folium import st_folium
+import json
+from geopandas.tools import geocode
