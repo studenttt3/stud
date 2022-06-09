c95044842933d5faa9f5d9b21463d33682fb8453
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-09 11:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ import json
 from geopandas.tools import geocode
 
 books = pd.read_csv("books.csv")
+books['num_pages'] = int(books['num_pages'])
 
 cat = st.selectbox(
 "Category", books["categories"].value_counts().index)
 df_selection = books[lambda x: x["categories"] == cat]
 df_selection =  df_selection.sort_values('average_rating', ascending = False)
-df_selection = df_selection[['title','subtitle', 'average_rating', 'num_pages', 'published_year']]
+df_selection = df_selection[['title', 'average_rating', 'num_pages', 'published_year']]
 df_selection[0:10]
