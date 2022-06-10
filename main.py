8f0bc76647a5e78898aa777194890ef4ff0a4a4d
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-10 20:37:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,9 @@ st.markdown("This book was written by " + aut + ". You can learn more about him/
 st.markdown("Also you can read description of this book below.")
 st.markdown(need['description'][0:1].values[0])
 
-'''
+
 url_pic = need['image_url'][0:1].values[0]
+'''
 r = get.requests(url_pic)
 out = open("...\img.jpg", 'wb')
 out.write(resource.content())
@@ -76,6 +77,8 @@ out.close()
 imga =Image.open("...\img.jpg")
 imga.show
 '''
+st.image(url_pic)
+
 which_bs = st.radio("", ('Наибольшая концентрация книжных магазинов','Наименьшая концентрация книжных магазинов'))
 
 if(which_bs == "Наибольшая концентрация книжных магазинов"):
