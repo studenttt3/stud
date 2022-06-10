98407e89588cad12df4ec21b24b22ba1ccae1b80
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-10 16:39:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ for i in range(ln):
         aut = aut + list[i] + " and"
     else:
         aut = aut + list[i]
-st.markdown("This book was written by " + aut)
+st.markdown("This book was written by " + aut + ". You can learn more about him/her or them at this link" + need['wiki_url'][0:1].values[0] + ".")
 
 ##driver = webdriver.Chrome('/Users/godun/Downloads/chromedriver_win32 (1)/chromedriver')
 ##driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
