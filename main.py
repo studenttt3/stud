dd81ececb1a752a70c67e3f56885e3cad4caab97
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-13 12:39:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ with st.echo(code_location='below'):
     
     shop = st.selectbox("Название", mos['Name'].unique())
     need_1 = mos[lambda x: x["Name"] == shop]
-    map3 = folium.Map(location=[55.7522, 37.6156], zoom_start = 10.4)
+    map3 = folium.Map(location=[55.7522, 37.6156], zoom_start = 10.45)
     for i in range(len(need_1.index)):
         ad = mos['Address'][i:i + 1].values[0]
         street = re.split("[,]", ad)[1]
