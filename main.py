12db30351ef148e6335c28991cd541ac66d21702
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-06-13 12:36:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ with st.echo(code_location='below'):
     
     shop = st.selectbox("Название", mos['Name'].unique())
     need_1 = mos[lambda x: x["Name"] == shop]
-    map3 = folium.Map(location=[55.7522, 37.6156], zoom_start = 11)
+    map3 = folium.Map(location=[55.7522, 37.6156], zoom_start = 10)
     for i in range(len(need_1.index)):
         ad = mos['Address'][i:i + 1].values[0]
         street = re.split("[,]", ad)[1]
@@ -226,7 +226,6 @@ with st.echo(code_location='below'):
         folium.Marker(location = [lat, lon], popup = str(add)).add_to(map3)
     st_data3 = st_folium(map3, width = 750)
 
-    st.write(lat, lon)
 ##driver = webdriver.Chrome('/Users/godun/Downloads/chromedriver_win32 (1)/chromedriver')
 ##driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 ##driver.get(url)
